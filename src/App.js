3629c494b1ecf8cd5aa6efdbec8ce8e11c2caa49
samuel <osamaimafidon@gmail.com>
2019-09-27 00:37:21 +0100

    created Game components for view

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Tile from './components/UI/Tiles/Tile';
 import Score from './components/UI/Scores/Score';
-import Timer from './components/Timer/Timer';
+// import Timer from './components/Timer/Timer';
+import Game from './views/Game/Game';
 import './App.css';
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
     <div className="App">
       <Tile letter="A" />
       <Score name="me" score="50" />
-      <Timer />
+      {/* <Timer /> */}
+      <Game />
     </div>
   );
 }
--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,10 +1,29 @@
-import React, { useState, useEffect } from "react";
-import "./timer.css";
+import React, { useState,useEffect } from "react";
+import './timer.css';
 
-const Game = ({ startTimer }) => {
-  
+const Timer = ({ startTime }) => {
+  const [timer, setTimer] = useState("10");
+
+  function start() {
+    setTimeout(handleIncrementTimer, 1000);
+  }
+
+  useEffect(start);
+  function handleIncrementTimer() {
+    let sec = timer;
+    let secInNum = Number(sec)
+
+    if (secInNum !== 0) {
+      secInNum -= 1;
+      if (secInNum < 10) sec = "0" + secInNum;
+      else sec = secInNum;
+    }
+
+    setTimer(sec);
+  }
+
+  return <div className="timer">{timer}</div>;
+};
 
 
-}
-
-export default Game;
+export default Timer;
