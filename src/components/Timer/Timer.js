c955df346c73b28404199b2f8fa3e0ef0deaef83
samuel <osamaimafidon@gmail.com>
2019-09-26 16:58:49 +0100

    Modified timer to be countdown timer only

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,44 +1,10 @@
-import React, { useState,useEffect } from "react";
-import './timer.css';
+import React, { useState, useEffect } from "react";
+import "./timer.css";
 
-const Timer = ({ startTime }) => {
-  const [timer, setTimer] = useState("00:00");
-
-  function start() {
-    setTimeout(handleIncrementTimer, 1000);
-  }
-
-//   useEffect(start);
-  function handleIncrementTimer() {
-    const minAndSec = timer.split(":");
-    const sec = minAndSec[1];
-    const min = minAndSec[0];
-
-    let secInNum = Number(sec);
-    let minInNum = Number(min);
-
-    if (secInNum < 60) {
-      secInNum += 1;
-      if (secInNum < 10) minAndSec[1] = "0" + secInNum;
-      else minAndSec[1] = secInNum;
-    } else {
-      minAndSec[1] = "00";
-      if (minInNum < 60) {
-        minInNum += 1;
-        if (minInNum < 10) minAndSec[0] = "0" + minInNum;
-        else minAndSec[0] = minInNum;
-      }
-      secInNum = 0;
-      minInNum += 1;
-    }
-
-    const newTime = `${minAndSec[0]}:${minAndSec[1]}`;
-
-    setTimer(newTime);
-  }
-
-  return <div className="timer">{timer}</div>;
-};
+const Game = ({ startTimer }) => {
+  
 
 
-export default Timer;
+}
+
+export default Game;
