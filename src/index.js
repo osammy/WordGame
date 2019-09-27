ee6e6fffac25e99f00bd3e514dece49a18d30edd
samuel <osamaimafidon@gmail.com>
2019-09-27 21:04:43 +0100

    added Home.js and changed routing to be imported from an array

--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
+// import "./index.css";
+// import App from "./App";
+// import * as serviceWorker from "./serviceWorker";
+
+// ReactDOM.render(
+//   <Router>
+//     <App />
+//   </Router>,
+//   document.getElementById("root")
+// );
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+// import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
+import './index.css';
+
+import indexRoutes from "./routes/index.js";
+
 
 ReactDOM.render(
   <Router>
-    <App />
+    <Switch>
+      {indexRoutes.map((prop, key) => {
+        return <Route  exact={prop.exact}  path={prop.path} component={prop.component} key={key} />;
+      })}
+    </Switch>
   </Router>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
